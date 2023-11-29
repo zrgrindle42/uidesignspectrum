5188674525b1fb9eca681301d9990da2d74613f0
jzt0059 <152354494+jzt0059@users.noreply.github.com>
2023-11-28 22:09:45 -0600

    Add files via upload

---
+++ geographyLandingScreen.js
@@ -0,0 +1,18 @@
+﻿window.onload=function () {
+    document.getElementById('geography_class').addEventListener('click', geographyTopics);
+
+}
+
+function statsTopics() {
+  document.getElementById("course_1_item_1").textContent = 'Introduction';
+  document.getElementById("course_1_item_2").textContent = 'Physical Geography';
+  document.getElementById("course_1_item_3").textContent = 'Human Geography';
+  document.getElementById("course_1_item_4").textContent = 'Physics';
+  document.getElementById("course_1_item_5").textContent = 'Geopolitics';
+  document.getElementById("course_1_item_6").textContent = 'Economic Geography';
+  document.getElementById("course_2_item_1").textContent = 'Cartography & Map Skills';
+  document.getElementById("course_2_item_2").textContent = 'Environmental Geography';
+  document.getElementById("course_2_item_3").textContent = 'Cultural Geography';
+  document.getElementById("course_2_item_4").textContent = 'Regional Studies';
+  document.getElementById("course_2_item_5").textContent = 'Geography of Health';
+  document.getElementById("course_2_item_6").textContent = 'Tourism Geography';
